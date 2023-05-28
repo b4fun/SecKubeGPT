c66de7746463d2d45d176474e23f141aaabbd1cf
hbc <bcxxxxxx@gmail.com>
2023-05-28 14:19:39 -0700

    fix: get_event_loop again

--- utils.py
+++ utils.py
@@ -47,4 +47,4 @@ def patch_script_thread_eventloop_if_needed():
         if "There is no current event loop in thread" in str(ex):
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            return asyncio.get_running_loop()
+            return asyncio.get_event_loop()
